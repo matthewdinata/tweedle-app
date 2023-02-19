9bcd75739155aa25b4ec3cfd532f993ac51dcdda
mattdinata <matt.dinata@gmail.com>
2023-02-19 22:17:56 +0800

    [src] Install Google fonts and React Icons

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FiAtSign } from "react-icons/fi";
+
+export default function Logo() {
+  return (
+   <>
+      <div className="logo">
+         <FiAtSign />
+         <h1 className="logo__title">Tweedle</h1>
+      </div>
+   </>
+  )
+}
---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Logo from '../../components/Logo'
+
+export default function Landing() {
+   return(<Logo/>)
+}
