22a20ce67d84ec60bdae16f43207d521dda84d05
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-02-19 22:38:42 +0800

    [tailwind.config] Set up color variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      red: '#DC5C66',
+      black: {
+        100: '#2E2E2E',
+        200: '#1E1E1E',
+        300: '#000000',
+      },
+      violet: '#8B7AD6',
+      purple: '#AF67E6',
+      teal: '#418CAE',
+    },
     extend: {
       fontFamily: {
-        inter: ['Inter']
-      }
+        inter: ['Inter'],
+      },
     },
   },
   plugins: [],
