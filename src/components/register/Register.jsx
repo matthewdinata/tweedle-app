419dc4a327cafe8f7d27f602afe812cfd509b949
mattdinata <matt.dinata@gmail.com>
2023-02-22 19:16:21 +0800

    [Landing] UI for Register

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Register.css'
 
+// assets
+import { FcGoogle } from "react-icons/fc"
+
 export default function Register() {
   const handleSubmit = async (e) => {
 
@@ -11,6 +14,13 @@ export default function Register() {
       <input type="email" placeholder="Enter email address" />
       <input type="password" placeholder="Enter password" />
       <input type="text" placeholder="Enter username" /> {/* yup validation */}
+      <button className="register__button">Register</button>
+      <div className="register__text-continue font-medium flex justify-between items-center mb-4">
+        <div className="continue-google__line h-px bg-white w-20"></div>
+        or continue with
+        <div className="continue-google__line h-px bg-white w-20"></div>
+      </div>
+      <button className="register__button-continue flex items-center justify-center gap-3 font-medium"><FcGoogle className="h-6 w-auto"/>Sign in with Google</button>
     </form>
   )
 }
