6345d2fba5e810158c9464229674c624ea6c501a
Matthew Dinata <matt.dinata@gmail.com>
2023-02-22 11:35:45 +0800

    [Register] Update form: Continue with Google

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -13,6 +13,12 @@ export default function Register() {
       <input type="password" placeholder="Enter password" />
       <input type="text" placeholder="Enter username" /> {/* yup validation */}
       <button className="register__button">Register</button>
+      <div className="register__text-continue font-medium flex justify-between items-center mb-4">
+        <div className="continue-google__line h-px bg-white w-20"></div>
+        or continue with
+        <div className="continue-google__line h-px bg-white w-20"></div>
+      </div>
+      <button className="register__button-continue">Sign in with Google</button>
     </form>
   )
 }
