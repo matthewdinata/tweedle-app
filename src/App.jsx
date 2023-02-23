140324293d39b5362fb2c7d1ebca8722ce17872e
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-02-23 22:44:34 +0800

    [Sidebar] Create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 import Landing from './pages/landing/Landing'
+import Chat from './pages/chat/Chat'
+import Sidebar from './components/Sidebar'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Landing/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/chat' element={<Sidebar />} />
+      </Routes>
+    </Router>
   )
 }
 
