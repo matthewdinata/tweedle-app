691b5ce594a486bc704a6ad778921bc6518b4f47
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-02-23 17:26:07 +0800

    [Sign In] Create Sign In component

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
-import './Register.css'
+import './SignIn.css'
 
 // assets
 import { FcGoogle } from 'react-icons/fc'
 
-export default function Register() {
+export default function SignIn() {
   const handleSubmit = async (e) => {}
 
   return (
-    <form className='register' onSubmit={handleSubmit}>
+    <form className='signin' onSubmit={handleSubmit}>
       <input type='email' placeholder='Enter email address' />
       <input type='password' placeholder='Enter password' />
-      <input type='text' placeholder='Enter username' /> {/* yup validation */}
-      <button className='register__button'>Register</button>
-      <div className='register__text-continue font-medium flex justify-between items-center mb-4'>
+      <button className='signin__button'>Sign In</button>
+      <div className='signin__text-continue font-medium flex justify-between items-center mb-4'>
         <div className='continue-google__line h-px bg-white w-20'></div>
         or continue with
         <div className='continue-google__line h-px bg-white w-20'></div>
       </div>
-      <button className='register__button-continue'>
+      <button className='signin__button-continue'>
         <FcGoogle className='h-6 w-auto' />
-        Register with Google
+        Sign in with Google
       </button>
     </form>
   )
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -9,22 +9,24 @@ import LandingImage from '../../assets/landing-image.png'
 // components
 import Register from '../../components/register/Register'
 import Logo from '../../components/logo/Logo'
-import LandingIllustration from '../../assets/LandingIllustration.png'
+import SignIn from '../../components/signIn/SignIn'
 
 export default function Landing() {
   return (
     <>
-      <div className='landing'>
+      <div className='landing w-[960px] m-auto'>
         <div className='landing__navbar'>
           <Logo />
           <div className='landing__navbar__link'>
-            <a className='link-active'>Sign in</a>
+            <a className='link--active'>Sign in</a>
             <a>Register</a>
           </div>
         </div>
-        <div className='landing__container'>
-          <img src={LandingIllustration} />
-          <div className='landing__form'></div>
+        <div className='landing__container flex justify-between items-center'>
+          <img className='landing__image h-[28rem]' src={LandingImage} />
+          <div className='landing__form py-auto'>
+            <Register />
+          </div>
         </div>
       </div>
     </>
