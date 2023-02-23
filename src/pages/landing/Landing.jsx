b51fae8c02b568165334947654c6e81ae5acf59a
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-02-23 17:25:41 +0800

    [Landing] Added landing illustration

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -4,47 +4,29 @@ import React from 'react'
 import './Landing.css'
 
 // assets
-import LandingImage from "../../assets/landing-image.png"
+import LandingImage from '../../assets/landing-image.png'
 
 // components
 import Register from '../../components/register/Register'
+import Logo from '../../components/logo/Logo'
+import LandingIllustration from '../../assets/LandingIllustration.png'
 
 export default function Landing() {
-<<<<<<< HEAD
-
-   return (
-      <>
-         <div className="landing w-[960px] m-auto">
-            <div className='landing__navbar text-white mb-20 flex justify-end gap-8 font-semibold pr-8 pt-24'>
-               <span>Sign In</span>
-               <span>Register</span>
-            </div>
-            <div className="landing__container flex justify-between">
-               <img className="landing__image h-[28rem] mb-10" src={LandingImage} />
-               <div className="landing__form">
-                  <Register />
-               </div>
-            </div>
-         </div>
-      </>
-   )
-=======
   return (
     <>
       <div className='landing'>
         <div className='landing__navbar'>
           <Logo />
           <div className='landing__navbar__link'>
-            <a>Sign in</a>
+            <a className='link-active'>Sign in</a>
             <a>Register</a>
           </div>
         </div>
         <div className='landing__container'>
-          <img />
+          <img src={LandingIllustration} />
           <div className='landing__form'></div>
         </div>
       </div>
     </>
   )
->>>>>>> 753695b ([Landing] Added navigation bar)
 }
