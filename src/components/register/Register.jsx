3caf63110ff8c6683958197357cad1a807f7e8be
mattdinata <matt.dinata@gmail.com>
2023-02-27 16:48:27 +0700

    [Register] Update form validation using Yup and React-Hook-Form for Email/Password registration

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -17,55 +17,88 @@ import { userSchema } from '../../utils/UserValidation';
 
 export default function Register() {  
   const [error, setError] = useState(false)
+  const [processing, setProcessing] = useState(false)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let formData = {
-      email: e.target[0].value,
-      password: e.target[1].value,
-      confirmPassword: e.target[2].value,
-      username: e.target[3].value
-    };
-    const isValid = await userSchema.isValid(formData);
-    console.log(isValid);
+  // integrate React-Hook-Form with Yup validation
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(userSchema),
+  });
+  const { errors } = formState;
 
+  // Email/Password sign in method
+  const onSubmit = async (userData) => {
+    setProcessing(true)
     try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
-
+      const res = await createUserWithEmailAndPassword(auth, userData.email, userData.password)
       await setDoc(doc(db, "users", res.user.uid), {
         uid: res.user.uid,
-        email,
-        username,
+        email: userData.email,
+        username: userData.username,
+        displayName: userData.username,
+        profilePic: undefined,
       }); 
-    } 
+    }
     catch (error) {
+      setProcessing(false)
       setError(true)
       console.log(error.message)
     }
-  }
+    setProcessing(false)
+  };
 
+  // Google sign in method
   const handleClick = async (e) => {
     e.preventDefault();
+    setProcessing(true)
     try {
       const res = await signInWithPopup(auth, GoogleProvider);
+      console.log(res)
+      await setDoc(doc(db, "users", res.user.uid), {
+        uid: res.user.uid,
+        email: res.user.email,
+        username: res.user.username,
+        displayName: res.user.displayName,
+        profilePic: res.user.photoURL,
+      }); 
     }
     catch (error) {
+      setProcessing(false)
       setError(true)
       console.log(error.message)
     }
+    setProcessing(false)
   }
 
   return (
-    <form className='register' onSubmit={handleSubmit}>
+    <form className='register' onSubmit={handleSubmit(onSubmit)}>
 
       {/* register with Email/Password */}
-      <input type='email' name='email' placeholder='Enter email address' />
-      <input type='password' name='password' placeholder='Enter password' />
-      <input type='password' name='confirmPassword' placeholder='Confirm password' />
-      <input type='text' name='username' placeholder='Enter username' />
+
+      <div className={`register__container ${errors.email ? 'register__container--invalid' : ''}`}>
+        <input type='email' name='email' placeholder='Enter email address' {...register('email')} />
+        <span className="register__invalid-message">{errors.email ? errors.email.message : ""}</span>
+      </div>
+
+      <div className={`register__container ${errors.password ? 'register__container--invalid' : ''}`}>
+        <input type='password' name='password' placeholder='Enter password' {...register('password')} />
+        <span className="register__invalid-message">{errors.password?.message}</span>
+      </div>
+
+      <div className={`register__container ${errors.confirmPassword ? 'register__container--invalid' : ''}`}>
+        <input type='password' name='confirmPassword' placeholder='Confirm password' {...register('confirmPassword')} />
+        <span className="register__invalid-message">{errors.confirmPassword?.message}</span>
+      </div>
+
+      <div className={`register__container ${errors.username ? 'register__container--invalid' : ''}`}>
+        <input type='text' name='username' placeholder='Enter username' {...register('username')} />
+        <span className="register__invalid-message">{errors.username?.message}</span>
+      </div>
+      
       <button className='register__button'>Register</button>
 
+
       {/* continue with Google option */}
+
       <div className='register__text-continue font-medium flex justify-between items-center mb-4'>
         <div className='continue-google__line h-px bg-white w-20'></div>
         or continue with
