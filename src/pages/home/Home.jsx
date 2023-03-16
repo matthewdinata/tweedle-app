84e545cc9521ed7af4c9f44c4cefea9013bf9b5b
mattdinata <matt.dinata@gmail.com>
2023-03-16 16:38:29 +0800

    [Home] Finalise UI for Posts and PostInput

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,26 +4,29 @@ import React from 'react';
 import './Home.css';
 
 // hooks
-import { useUser } from '../../hooks/useUser';
+// import { useUser } from '../../hooks/useUser';
 
 // components
 import Posts from '../../components/posts/Posts';
 import PostInput from '../../components/postInput/PostInput';
+import Sidebar from '../../components/Sidebar';
 
 export default function Home() {
-  const { currentUser, logout } = useUser();
+  // const { currentUser, logout } = useUser();
   return (
     <div className='home'>
-      <div className='home__container flex flex-col items-center justify-center'>
-        <Posts />
+      <Sidebar />
+      <div className='home__container flex flex-col items-center justify-center gap-7'>
         <PostInput />
+        <Posts />
       </div>
-      <h1 className='text-white'>{currentUser.uid}</h1>
+      {/* <h1 className='text-white'>{currentUser.displayName}</h1>
       <button
         onClick={logout}
         className='block border-none bg-purple bg-opacity-90 focus:bg-opacity-100 hover:bg-opacity-100 px-4 h-16 mb-4 font-medium rounded-lg text-white focus:outline-none transition-all ease-in-out duration-500'
       >
         Logout
-      </button>
+      </button> */}
     </div>
   );
+}
