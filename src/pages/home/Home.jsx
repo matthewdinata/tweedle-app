81632903db2816e60c0366da636003ae40daba23
mattdinata <matt.dinata@gmail.com>
2023-03-20 20:20:19 +0800

    [Edit] Setup UI for Edit page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,13 +13,6 @@ export default function Home() {
         <PostInput />
         <Posts />
       </div>
-      {/* <h1 className='text-white'>{currentUser.displayName}</h1>
-      <button
-        onClick={logout}
-        className='block border-none bg-purple bg-opacity-90 focus:bg-opacity-100 hover:bg-opacity-100 px-4 h-16 mb-4 font-medium rounded-lg text-white focus:outline-none transition-all ease-in-out duration-500'
-      >
-        Logout
-      </button> */}
     </div>
   );
 }
