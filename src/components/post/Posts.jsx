aeb82ff32a69af45006f90bf006fd0581a96314f
mattdinata <matt.dinata@gmail.com>
2023-03-20 22:43:01 +0800

    [home] Clean old function (getUserData)

--- src/components/post/Posts.jsx
+++ src/components/post/Posts.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 // hooks
-import { useUser } from '../../hooks/useUser';
-import { useUserData } from '../../hooks/useUserData';
+import { useAuth } from '../../hooks/useAuth';
 
 // services
 import { doc, onSnapshot } from 'firebase/firestore';
@@ -12,30 +11,19 @@ import { db } from '../../firebase';
 import Post from './Post';
 
 export default function Posts() {
-  const [posts, setPosts] = useState([]);
   const [friends, setFriends] = useState([]);
-
-  // get complete user data from Firestore Database using useUserData hook
-  const [user, setUser] = useState(null);
-  const { currentUser } = useUser();
-  const { getUserData } = useUserData();
-  useEffect(() => {
-    const getCompleteUser = async () => {
-      const res = await getUserData(currentUser.uid);
-      setUser(res);
-    };
-    getCompleteUser();
-  }, []);
+  const { currentUid } = useAuth();
 
   // get list of friends
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, 'friends', user.uid), (doc) => {
+    const unsub = onSnapshot(doc(db, 'friends', currentUid), (doc) => {
       doc.exists() && setFriends(doc.data().listOfFriends);
     });
     return () => {
       unsub();
     };
   }, []);
+  console.log(friends);
 
   return (
     <div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
-// hooks
-import { useAuth } from '../../hooks/useAuth';
-
 // components
 import Posts from '../../components/post/Posts';
 import PostInput from '../../components/post/PostInput';
 import Sidebar from '../../components/Sidebar';
 
-export default function Home() {
-  const { currentUid, logout } = useAuth();
+// hooks
+import { useAuth } from '../../hooks/useAuth';
 
+export default function Home() {
+  const { logout } = useAuth();
   return (
     <div className='home grid grid-cols-[20rem,auto]'>
       <Sidebar />
       <div className='home__container flex flex-col items-center justify-start gap-7 my-10'>
         <PostInput />
         <Posts />
+        <button
+          onClick={logout}
+          className='block border-none bg-purple bg-opacity-90 focus:bg-opacity-100 hover:bg-opacity-100 px-4 h-16 mb-4 font-medium rounded-lg text-white focus:outline-none transition-all ease-in-out duration-500'
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
