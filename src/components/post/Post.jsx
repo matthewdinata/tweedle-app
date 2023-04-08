c1df41b138d7d1646cde151b88d4574deec49b0d
mattdinata <matt.dinata@gmail.com>
2023-04-09 00:27:43 +0800

    [Post] Add function to display poster data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -8,8 +8,14 @@ import { doc, getDoc } from 'firebase/firestore';
 // assets
 import { FiHeart } from 'react-icons/fi';
 
+// hooks
+import { useUserData } from '../../hooks/useUserData';
+
 export default function Post({ postId }) {
   const [post, setPost] = useState(null);
+  const [poster, setPoster] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { getUserData } = useUserData();
 
   // get specific post
   const getPost = async (postId) => {
@@ -19,47 +25,60 @@ export default function Post({ postId }) {
   };
 
   useEffect(() => {
-    getPost(postId).then((res) => {
-      setPost(res);
-    });
+    const initiatePost = async () => {
+      const postResponse = await getPost(postId);
+      setPost(postResponse);
+
+      const posterResponse = await getUserData(postResponse.posterId);
+      setPoster(posterResponse);
+    };
+    initiatePost();
+    setLoading(false);
   }, []);
 
-  console.log(post);
+  console.log(post, poster);
 
   return (
-    <div className='posts w-[680px] h-auto bg-black-100 rounded-lg p-6 flex flex-col gap-4'>
-      <div className='posts__container flex justify-between items-start'>
-        <div className='flex'>
-          <img
-            className='bg-black rounded-full w-10 h-10'
-            src='..'
-          ></img>
-          <div className='container__profile flex flex-col items-start ml-5 justify-center'>
-            <span className='text-white my-[-4px] font-medium text-base'>
-              John Doe
-            </span>
-            <span className='text-white font-light text-xs'>@johndoe</span>
-          </div>
-        </div>
-        <span className='text-white text-opacity-50 text-xs'>4m</span>
-      </div>
-      <div className='posts__container overflow-hidden flex flex-col gap-4'>
-        <span className='text-white text-sm'>{post.text}</span>
-        {post.img && (
-          <div className='container__img rounded-lg max-h-96 max-w-full'>
-            <img
-              src={post.img}
-              alt=''
-              className='rounded-lg object-contain max-h-96 max-w-full'
-            />
+    !loading && (
+      <div className='posts w-[680px] h-auto bg-black-100 rounded-lg p-6 flex flex-col gap-4'>
+        {poster && (
+          <div className='posts__container flex justify-between items-start'>
+            <div className='flex'>
+              <img
+                className='bg-black rounded-full w-10 h-10'
+                src={poster.profilePic}
+              ></img>
+
+              <div className='container__profile flex flex-col items-start ml-5 justify-center'>
+                <span className='text-white my-[-4px] font-medium text-base'>
+                  {poster.displayName}
+                </span>
+                <span className='text-white font-light text-xs'>
+                  @{poster.username}
+                </span>
+              </div>
+            </div>
+            <span className='text-white text-opacity-50 text-xs'>4m</span>
           </div>
         )}
+        <div className='posts__container overflow-hidden flex flex-col gap-4'>
+          <span className='text-white text-sm'>{post?.text}</span>
+          {post?.img && (
+            <div className='container__img rounded-lg max-h-96 max-w-full'>
+              <img
+                src={post.img}
+                alt=''
+                className='rounded-lg object-contain max-h-96 max-w-full'
+              />
+            </div>
+          )}
+        </div>
+        <div className='posts__likes flex justify-end text-white text-sm text-opacity-75 font-medium items-center gap-2'>
+          <span>189</span>
+          <FiHeart className='text-xl' />
+        </div>
       </div>
-      <div className='posts__likes flex justify-end text-white text-sm text-opacity-75 font-medium items-center gap-2'>
-        <span>189</span>
-        <FiHeart className='text-xl' />
-      </div>
-    </div>
+    )
   );
 }
 
