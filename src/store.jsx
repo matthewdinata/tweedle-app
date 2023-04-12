82502750ccc8ea84c07d0a3c62924ae31f28bc36
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-04-12 14:11:50 +0800

    [src] Create chat reducer

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
 import friendsReducer from './features/friendsSlice';
+import chatReducer from './features/chatSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     friends: friendsReducer,
+    chat: chatReducer,
   },
 });
